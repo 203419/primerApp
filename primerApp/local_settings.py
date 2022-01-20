a1618d5d4549f86f874394e463347c62e101c0d8
203419 <203419@ids.upchiapas.edu.mx>
2022-01-20 17:00:04 -0600

    ¡add file local_settings

--- primerApp/local_settings.py
+++ primerApp/local_settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-c!9#351k8*w#liz=)t_q*5a5x%)4iv#nxfg-ssr$y6xvtnh$zo'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+SECRET_KEY = 'django-insecure-c!9#351k8*w#liz=)t_q*5a5x%)4iv#nxfg-ssr$y6xvtnh$zo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +80,17 @@ WSGI_APPLICATION = 'primerApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbDjango',
+        'USER': 'postgres',
+        'PASSWORD': 'sebasramirez11',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,8 +132,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-try:
-    from primerApp.local_settings import *
-except ImportError :
-    pass
